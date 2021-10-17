559a4d2cedf44ea9e68339b9923ec4c386f7baaa
siryus <siryus@csie.io>
2021-10-17 15:22:35 +0800

    template

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontend/index');
+})->name('home');
+
+Route::get('about', function () {
+    return view('frontend/about');
+})->name('about');
+
+Route::get('products', function () {
+    return view('frontend/products');
+})->name('products');
+
+Route::get('store', function () {
+    return view('frontend/store');
+})->name('store');
